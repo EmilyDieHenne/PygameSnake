48743ceb4ad06a72bd08551f97640411c8f1d757
Falk Abschlag <falkabschlag@gmx.de>
2023-06-27 20:12:14 +0200

    added snake movement :3

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import pygame
 
-pygame.display.set_caption("Snake")
 
-WIDTH, HEIGHT = 800, 600
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-WINDOW.fill((0, 0, 0))
-pygame.display.update()
-pygame.draw.rect(WINDOW, (255, 255, 255), (50, 50, 700, 500))
-pygame.display.update()
-
-DIRECTIONS = ['up', 'right', 'down', 'left']
-direction = 0
+direction = 'up'
 snake = [
-    [5, 5],
-    [5, 6],
-    [5, 7],
+    {'x': 5, 'y': 5},
+    {'x': 5, 'y': 6},
+    {'x': 5, 'y': 7},
 ]
 score = 0
 
+WIDTH, HEIGHT = 800, 600
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Snake")
+
 
 def move():
-    length = len(snake)
+
+    eat_fruit = False
+
+    if not eat_fruit:
+        snake.pop()
+
+    next_head_x = snake[0]['x']
+    next_head_y = snake[0]['y']
+
+    if direction == 'up':
+        next_head_y = next_head_y - 1
+    elif direction == 'down':
+        next_head_y = next_head_y + 1
+    elif direction == 'right':
+        next_head_x = next_head_x + 1
+    elif direction == 'left':
+        next_head_x = next_head_x - 1
+
+    snake.reverse()
+    snake.append({'x': next_head_x, 'y': next_head_y})
+    snake.reverse()
 
 
 def draw():
+    WINDOW.fill((0, 0, 0))
+    pygame.draw.rect(WINDOW, (255, 255, 255), (50, 50, 700, 500))
     for body in snake:
         pygame.draw.rect(
             WINDOW,
             (0, 0, 255),
-            (25 + 50 * body[0], 25 + 50 * body[1], 50, 50)
+            (50 + 50 * body['x'], 50 + 50 * body['y'], 50, 50)
         )
     pygame.display.update()
 
@@ -41,7 +58,7 @@ def main():
     while running:
         clock.tick(60)
         for event in pygame.event.get():
-            print(event)
+            #print(event)
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.USEREVENT:
