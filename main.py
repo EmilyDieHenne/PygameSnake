49288b525a56b263ad6acad689f8060dadd058ef
Falk Abschlag <falkabschlag@gmx.de>
2023-06-30 14:52:53 +0200

    added eatable food game now runs on fullscreen

--- main.py
+++ main.py
@@ -1,36 +1,66 @@
 import pygame
+import random
 
-snake = [
-    {'x': 5, 'y': 5},
-    {'x': 5, 'y': 6},
-    {'x': 5, 'y': 7},
-]
-score = 0
+WINDOW = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+WIDTH, HEIGHT = WINDOW.get_size()
 
-WIDTH, HEIGHT = 800, 600
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake")
+colors = {
+    "bg_color": (0, 0, 0),
+    "border_color": (255, 255, 255),
+    "tile_color": (0, 100, 0),
+    "snake_color": (26, 187, 249),
+    "food_color": (255, 121, 249),
+}
 
 
-def check_for_collision():
+def draw(snake, food):
+    # A board is 40 tiles tall, and 40 tiles wide.
+    # Around the board is a border of one tile.
+    tile_size = HEIGHT / 42
+    spacing = (WIDTH - HEIGHT) / 2
+    board_size = HEIGHT - 2 * tile_size
+
+    WINDOW.fill(colors["bg_color"])
+
+    pygame.draw.rect(WINDOW, colors["border_color"], (spacing, 0, HEIGHT, HEIGHT))
+    pygame.draw.rect(WINDOW, colors["tile_color"], (spacing + tile_size, tile_size, board_size, board_size))
+
+    for body in snake:
+        pygame.draw.rect(
+            WINDOW,
+            colors["snake_color"],
+            (spacing + tile_size + tile_size * body['x'], tile_size + tile_size * body['y'], tile_size, tile_size)
+        )
+
+    pygame.draw.rect(
+        WINDOW,
+        colors["food_color"],
+        (spacing + tile_size + tile_size * food['x'], tile_size + tile_size * food['y'], tile_size, tile_size)
+    )
+
+    pygame.display.update()
+
+
+def check_for_collision(snake):
     # checks if snake head has collided with body
     if snake.count((snake[0])) > 1:
         return True
+
     # checks if snake head has hit the wall
-    if snake[0]['x'] < 0 or snake[0]['y'] < 0 or snake[0]['x'] > 13 or snake[0]['y'] > 9:
+    if snake[0]['x'] < 0 or snake[0]['y'] < 0 or snake[0]['x'] > 39 or snake[0]['y'] > 39:
         return True
 
     return False
 
 
-def move(direction):
-    eat_fruit = False
-
-    if not eat_fruit:
-        snake.pop()
-
+def move(direction, snake, food):
     next_head_x = snake[0]['x']
     next_head_y = snake[0]['y']
+
+    if not food == {'x': next_head_x, 'y': next_head_y}:
+        snake.pop()
+    else:
+        food.update({'x': random.randint(0, 39), 'y': random.randint(0, 39)})
 
     if direction == 'up':
         next_head_y = next_head_y - 1
@@ -46,28 +76,25 @@ def move(direction):
     snake.reverse()
 
 
-def draw():
-    WINDOW.fill((0, 0, 0))
-    pygame.draw.rect(WINDOW, (255, 255, 255), (50, 50, 700, 500))
-    for body in snake:
-        pygame.draw.rect(
-            WINDOW,
-            (0, 0, 255),
-            (50 + 50 * body['x'], 50 + 50 * body['y'], 50, 50)
-        )
-    pygame.display.update()
-
-
 def main():
+    snake = [
+        {'x': 5, 'y': 5},
+        {'x': 5, 'y': 6},
+        {'x': 5, 'y': 7},
+    ]
+    food = {'x': 2, 'y': 3}
+    score = 0
+
+    pygame.display.set_caption("Snake")
     direction = 'up'
     clock = pygame.time.Clock()
-    pygame.time.set_timer(pygame.USEREVENT, 500 - score)
+    pygame.time.set_timer(pygame.USEREVENT, 250 - score)
     running = True
-    draw()
+    draw(snake, food)
 
     while running:
         clock.tick(60)  # FPS
-        
+
         for event in pygame.event.get():
             # print(event)
             if event.type == pygame.KEYDOWN:
@@ -86,12 +113,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.USEREVENT:
-                move(direction)
-                has_collided = check_for_collision()
+                move(direction, snake, food)
+                has_collided = check_for_collision(snake)
                 if has_collided:
                     running = False
 
-                draw()
+                draw(snake, food)
 
     pygame.quit()
 
